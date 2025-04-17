5efb952c10348f36e7f0609391eebbc5b3af3920
JaeHwan <wpghks56@naver.com>
2025-04-17 17:34:53 +0900

    [Feat] dependency injection setup with GetIt

---
+++ lib/core/di/di_setup.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+import 'package:image_search/data/data_source/pixabay_data_source_impl.dart';
+import 'package:image_search/data/repository/pixabay_repository_impl.dart';
+import 'package:image_search/domain/data_source/pixabay_data_source.dart';
+import 'package:image_search/domain/repository/pixabay_respository.dart';
+import 'package:image_search/presentaion/detail/pixabay_detail_view_model.dart';
+import 'package:image_search/presentaion/search/search_pixabay_view_model.dart';
+
+final getIt = GetIt.instance;
+
+void di() {
+  getIt.registerSingleton<PixabayDataSource>(PixabayDataSourceImpl());
+  getIt.registerSingleton<PixabayRespository>(PixabayRepositoryImpl(getIt()));
+  getIt.registerFactory<PixabayDetailViewModel>(() {
+    return PixabayDetailViewModel(getIt());
+  });
+  getIt.registerFactory<SearchPixabayViewModel>(() {
+    return SearchPixabayViewModel(getIt());
+  });
+}
