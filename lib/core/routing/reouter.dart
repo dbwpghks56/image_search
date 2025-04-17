9109eb27359c4d230c64e04f19c45087677ee142
JaeHwan <wpghks56@naver.com>
2025-04-17 17:37:47 +0900

    [Fix] getit 타입 지정

--- lib/core/routing/reouter.dart
+++ lib/core/routing/reouter.dart
@@ -3,6 +3,10 @@ import 'package:image_search/core/di/di_setup.dart';
 import 'package:image_search/core/routing/routes.dart';
 import 'package:image_search/presentaion/detail/screen/pixabay_detail_root.dart';
 import 'package:image_search/presentaion/search/screen/search_pixabay_root.dart';
+import 'package:image_search/presentaion/search/search_pixabay_view_model.dart';
+
+import '../../presentaion/detail/pixabay_detail_view_model.dart'
+    show PixabayDetailViewModel;
 
 final router = GoRouter(
   initialLocation: Routes.search,
@@ -11,7 +15,7 @@ final router = GoRouter(
       path: Routes.search,
       builder: (context, state) {
         return SearchPixabayRoot(
-          viewModel: getIt(),
+          viewModel: getIt<SearchPixabayViewModel>(),
           onTileClick: (int value) {
             context.push('${Routes.detail}/$value');
           },
@@ -23,7 +27,10 @@ final router = GoRouter(
       builder: (context, state) {
         final int id = int.parse(state.pathParameters['id']!);
 
-        return PixabayDetailRoot(viewModel: getIt(), id: id);
+        return PixabayDetailRoot(
+          viewModel: getIt<PixabayDetailViewModel>(),
+          id: id,
+        );
       },
     ),
   ],
