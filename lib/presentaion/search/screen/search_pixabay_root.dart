33d8de25d665fb2e40d736162cc4131cc5a3d178
JaeHwan <wpghks56@naver.com>
2025-04-17 17:33:04 +0900

    [Feat] search screen components for Pixabay image search

---
+++ lib/presentaion/search/screen/search_pixabay_root.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:image_search/presentaion/search/screen/search_pixabay_screen.dart';
+import 'package:image_search/presentaion/search/search_pixabay_view_model.dart';
+
+class SearchPixabayRoot extends StatefulWidget {
+  final SearchPixabayViewModel viewModel;
+  final void Function(int value) onTileClick;
+  const SearchPixabayRoot({
+    super.key,
+    required this.viewModel,
+    required this.onTileClick,
+  });
+
+  @override
+  State<SearchPixabayRoot> createState() => _SearchPixabayRootState();
+}
+
+class _SearchPixabayRootState extends State<SearchPixabayRoot> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose;
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListenableBuilder(
+      listenable: widget.viewModel..findPixabiesByQuery(_controller.text),
+      builder: (context, _) {
+        return SearchPixabayScreen(
+          state: widget.viewModel.value,
+          onSearchClick: () {
+            widget.viewModel.findPixabiesByQuery(_controller.text);
+          },
+          onTileClick: widget.onTileClick,
+          controller: _controller,
+        );
+      },
+    );
+  }
+}
---
+++ lib/presentaion/search/screen/search_pixabay_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:image_search/core/presentation/component/image_tile.dart';
+import 'package:image_search/core/presentation/component/search_input.dart';
+import 'package:image_search/presentaion/search/search_pixabay_state.dart';
+
+class SearchPixabayScreen extends StatelessWidget {
+  final SearchPixabayState state;
+  final void Function() onSearchClick;
+  final void Function(int value) onTileClick;
+  final TextEditingController controller;
+  const SearchPixabayScreen({
+    super.key,
+    required this.state,
+    required this.onSearchClick,
+    required this.onTileClick,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          spacing: 24,
+          children: [
+            SearchInput(
+              controller: controller,
+              icon: GestureDetector(
+                onTap: onSearchClick,
+                child: Icon(Icons.search, color: Colors.cyan),
+              ),
+              placeholder: 'Search',
+            ),
+            Expanded(
+              child:
+                  state.isLoading
+                      ? Center(child: CircularProgressIndicator())
+                      : state.pixabaies.isNotEmpty
+                      ? GridView.builder(
+                        itemCount: state.pixabaies.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisSpacing: 34,
+                              crossAxisSpacing: 34,
+                            ),
+                        itemBuilder: (BuildContext context, int index) {
+                          return GestureDetector(
+                            onTap: () {
+                              onTileClick(state.pixabaies[index].id);
+                            },
+                            child: ImageTile(
+                              image: state.pixabaies[index].imageUrl,
+                            ),
+                          );
+                        },
+                      )
+                      : Center(
+                        child: Text(
+                          '검색 결과가 없습니다.',
+                          style: TextStyle(fontSize: 32),
+                        ),
+                      ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
