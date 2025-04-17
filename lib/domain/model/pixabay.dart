c7e16fbcbea1d6bb85b919bccec34c4c2de1d241
JaeHwan <wpghks56@naver.com>
2025-04-17 15:57:01 +0900

    [Feat] pixabay model create

---
+++ lib/domain/model/pixabay.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'pixabay.freezed.dart';
+
+@freezed
+abstract class Pixabay with _$Pixabay {
+  const factory Pixabay({
+    required int id,
+    required List<String> tags,
+    required String imageUrl,
+    required String user,
+  }) = _Pixabay;
+}
